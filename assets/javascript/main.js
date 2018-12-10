42b880217670d6f0717d985e2e5b8e8168c36a4e
Zkinsk <zoolunk@gmail.com>
2018-12-10 18:05:17 -0500

    more tweaks

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,8 @@ var topicArr = ["cats", "puppies", "fish", "owls", "kids", "falling down", "skun
 var favArr = [];
 var buttonDel = false;
 var addFav = false;
+var rowCount = 0;
+var resetTopic = false;
 
 
 // giphy api key = 4hOSx38y08m8D16miIeYgpnQTT2nKkae
@@ -30,10 +32,17 @@ function buttonClick(){
         }else{
             let topic = $(this).attr("value");
             let offset = parseInt($(this).attr("offset"));
-            let gifNumber = $("#gifNumber").val();
+            let gifNumber = parseInt($("#gifNumber").val());
             callGihpy(topic, offset, gifNumber, 0);
-            offset = offset + gifNumber;
+            offset += gifNumber;
+            if (resetTopic){
+                console.log("true")
+                resetTopic = false;
+                offset = 0;
+            }
             $(this).attr("offset", offset);
+            console.log("Offset: " + offset);
+            
         }
     })
 }
@@ -44,27 +53,37 @@ function buttonDropDown(){
     });
 
     $(":button[value='clearGifs']").on("click", function(){
-        $("#gifBox").empty();
+        $(".gifCols").empty();
     })
-    $(":button[value='favGifs']").on("click", function(){
-        addFav = true;
+    $(":button[value='resetTopic']").on("click", function(){
+        resetTopic = true;
     })
     $(":button[value='showFavs']").on("click", function(){
-        $("#gifBox").empty();
+        $(".gifCols").empty();
         callGihpy(0, 0, 0, 1);
+    })
+    $(document).on("click", ".gifStar", function(){
+        if(($(this).attr("fave-state")) == "notfaved"){
+            // alert("not fav")
+            $(this).css("color", "red").attr("fave-state", "faved");
+            let fav = $(this).attr("gifID");
+            if(!favArr.includes(fav)){
+            favArr.push(fav);}
+
+        }
+        else{
+            $(this).css("color", "yellow").attr("fave-state", "notfaved");
+            let fav = $(this).attr("gifID");
+            favArr.indexOf(fav);
+            favArr.splice((favArr.indexOf(fav)), 1);
+        }
+    //    alert("click")
     })
 }
 
 function gifClick(){
     $(document).on("click", ".gif", function(){
         console.log(favArr)
-        if(addFav){
-            $(this).fadeTo(120, 0.65).fadeTo(120, 1.0);
-            addFav = false;
-            let fav = $(this).attr("gifID");
-            if(!favArr.includes(fav)){
-            favArr.push(fav);}
-        }else{
             let stop = $(this).attr("data-still");
             let anim = $(this).attr("data-animate");
             if ($(this).attr("data-state") === "stopped"){
@@ -74,7 +93,6 @@ function gifClick(){
                 $(this).attr("data-state", "stopped");
                 $(this).attr("src", stop);
             }
-        }
     });
 }
 
@@ -107,6 +125,22 @@ function addTopic(){
             $("#newTopic").val("");
         };
     })
+    $("#butnForm").on('keyup', function(event){ 
+        event.preventDefault();
+        let newT = $("#newTopic").val();
+        if(event.keyCode == 13){ 
+          event.preventDefault();
+          if (newT !== "" && !topicArr.includes(newT)){
+            topicArr.push(newT);
+            bottonPopulate(topicArr);
+            $("#newTopic").val("");
+        }else{
+            alert("try again");
+            $("#newTopic").val("");
+        };
+          
+        }
+      });
 
 }
 
@@ -131,16 +165,35 @@ function callGihpy(topic, offSet, gifNumber, type){
 }
 
 function drawGifs(giphyObj){
+    if( ($(".gifRow .gifCol0").is(":empty"))  && ($(".gifRow .gifCol1").is(":empty")) && ($(".gifRow .gifCol2").is(":empty")) ){
+        rowCount = 0
+        console.log("empty");
+    };
     for (let i = 0; i < giphyObj.data.length; i++){
-        let cGifurl = giphyObj.data[i].images.fixed_height_still.url
-        let cGifStill = giphyObj.data[i].images.fixed_height_still.url
-        let cGifAnimate = giphyObj.data[i].images.fixed_height.url
+        let cGifurl = giphyObj.data[i].images.fixed_width_still.url
+        let cGifStill = giphyObj.data[i].images.fixed_width_still.url
+        let cGifAnimate = giphyObj.data[i].images.fixed_width.url
         let gifID = giphyObj.data[i].id;
         let gifRating = giphyObj.data[i].rating;
+        let gifDiv = $("<div>").addClass("gifDiv");
+        let gifStar = $("<div>").addClass("gifStar fas fa-star");
+        // gifStar.attr({"fav-state": "notfaved", "gifID": gifID})
+        if (favArr.includes(gifID)){
+            gifStar.attr({"fav-state":"faved", "gifID": gifID});
+            gifStar.css("color", "red");
+        }else{gifStar.attr({"gifID": gifID});
+        gifStar.attr("fave-state", "notfaved")
+        };
         let cGif = $("<img>").addClass("gif");
         cGif.attr({"src": cGifurl, "data-still": cGifStill, "data-animate": cGifAnimate, "data-state": "stopped", "gifID": gifID});
         cGif.attr({"data-toggle": "tooltip", "data-placement": "top", "title": "Rated: " + gifRating});
-        $("#gifBox").prepend(cGif);
+        gifDiv.append(cGif, gifStar);
+        if (rowCount >= 3){
+            rowCount = 0
+        };
+
+        $(".gifRow .gifCol" + rowCount).prepend(gifDiv);
+        rowCount++
     }
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
